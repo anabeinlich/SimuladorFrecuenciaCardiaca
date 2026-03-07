84dd98f4e6d83316c60bcfd6f2fdec0b262b96dd
anabeinlich <ana.b.beinlich@gmail.com>
2026-03-07 03:55:56 -0300

    implementado menu principal

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController: MonoBehaviour
+{
+    public void BotonEmpezar()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void BotonSalir()
+    {
+        Debug.Log("Saliste del simulador");
+        Application.Quit();
+    }
+}
